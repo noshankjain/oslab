bed1c5548c6d865aa73816338261b3c046964877
noshankjain <noshankjain@gmail.com>
2025-06-11 22:14:03 +0530

    Update mvt.c

--- mvt.c
+++ mvt.c
@@ -1,26 +1,31 @@
-#include <stdio.h>
-
-int main(){
-	int i, m, n, tot, s[20];
-	printf("ENTER MEOMERY\n");
-	scanf("%d",&tot);
-	printf("Enter no of processes\n Enter mem for OS");
-	scanf("%d %d", &n, &m);
-	for (i = 0; i < n; i++){
-		printf("SIZE OF PROCESS %d", i);
-		scanf("%d", &s[i]);
-	}
-	tot = tot - m;
-	for (i = 0; i < n; i++){
-		if (tot >= s[i]){
-			printf("ALloted memory to process %d\n", i);
-			tot -= s[i];
-		}
-		else{
-			printf("Proccess %d i sblsockeljd", i);
-		}
-	}
-	printf("External fragmentation is %d\n", tot);
-
-	return 0;
-}	
+#include<stdio.h>
+int main()
+{
+ int i,n,m,tot,s[20];
+ printf("enter total memory size: \n");
+ scanf("%d",&tot);
+ printf("enter no of processes: \n");
+ scanf("%d",&n);
+ printf("enter memory of OS: \n");
+ scanf("%d",&m);
+ for(i=0;i<n;i++)
+ {
+  printf("enter size of process %d: ",i);
+  scanf("%d",&s[i]);
+ }
+ tot=tot-m;
+ for(i=0;i<n;i++)
+ {
+  if(tot>=s[i])
+  {
+   printf("allocate memory to process %d\n",i);
+   tot-=s[i];
+  }
+  else
+  {
+   printf("process %d is blocked\n",i);
+  }
+ }
+ printf("external fragmentation is %d\n",tot);
+ return 0;
+}
